c63cc18412820c1cb688696be67ddbb363037cb7
Gorka <gorkavillara@gmail.com>
2022-02-08 12:38:09 +0100

    Quitado el test

--- src/components/AdSense.jsx
+++ src/components/AdSense.jsx
@@ -11,7 +11,6 @@ const AdSense = () => {
       data-ad-client="ca-pub-7313475970210959"
       data-ad-slot="9893978370"
       data-ad-format="auto"
-      data-adtest="on"
       data-full-width-responsive="true"
     ></ins>
   );
