43c6dc2b472ccbb1a9b0f4d01dadb62ec9518f6f
Gorka <gorkavillara@gmail.com>
2022-02-20 13:25:36 +0100

    Cambiado keyboard

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -4,7 +4,7 @@ import { IoSend, IoBackspaceOutline, IoRefresh } from "react-icons/io5";
 const keys = [
   ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
   ["a", "s", "d", "f", "g", "h", "j", "k", "l", "ñ"],
-  ["z", "x", "c", "v", "b", "n", "m", "back", "send"],
+  ["send", "z", "x", "c", "v", "b", "n", "m", "back"],
 ];
 
 const Keyboard = ({
@@ -23,7 +23,7 @@ const Keyboard = ({
             <React.Fragment key={j}>
               {key === "back" && (
                 <button
-                  className="bg-slate-300 w-9 h-10 rounded-lg uppercase text-2xl flex justify-center items-center"
+                  className="bg-slate-300 border-b-4 border-slate-400 w-12 h-12 rounded-lg uppercase text-2xl flex justify-center items-center"
                   onClick={() => type(key)}
                 >
                   <span>
@@ -34,7 +34,7 @@ const Keyboard = ({
               {key === "send" && (
                 <button
                   key={j}
-                  className="bg-blue-600 text-white w-14 h-10 rounded-lg uppercase text-2xl flex justify-center items-center"
+                  className="bg-blue-600 text-white w-14 h-12 rounded-lg uppercase text-2xl flex justify-center items-center border-b-4 border-blue-800"
                   onClick={() => (status === "" ? type(key) : reset())}
                 >
                   <span>{status === "" ? <IoSend /> : <IoRefresh />}</span>
@@ -43,11 +43,11 @@ const Keyboard = ({
               {key !== "back" && key !== "send" && (
                 <button
                   key={j}
-                  className={`bg-slate-300
+                  className={`bg-slate-300 border-b-4 border-slate-400
                   ${failedKeys.includes(key) && "bg-slate-800 text-white"}
                   ${misplacedKeys.includes(key) && "bg-yellow-500"}
                   ${successKeys.includes(key) && "bg-green-600 text-white"}
-                  w-8 h-10 rounded-lg uppercase text-xl flex justify-center items-center`}
+                  w-8 h-12 rounded-lg uppercase text-xl flex justify-center items-center`}
                   onClick={() => type(key)}
                 >
                   <span>{key}</span>
